ae06f7a5c1247368bc09e923338ed2caef69bff1
Your Name <your@email.com>
2025-11-14 10:58:40 +0530

    Configure to build to docs folder for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/hytork-motors-website/',
+      base: '/HyTork/',
+      build: {
+        outDir: 'docs'
+      },
       server: {
         port: 3000,
         host: '0.0.0.0',
